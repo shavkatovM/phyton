15178c40241cd9cb7c89ad55fa88075515836755
shavkatovM <iscoiscobekov@gmail.com>
2025-11-29 17:54:19 +0500

    yangi fayl searching
    5ta masala to'liq ishlandi

---
+++ yengi.py
@@ -0,0 +1,12 @@
+#k = int(())
+#n = (int())
+#for i in range(n):
+#  print(k)
+#2a = int(input())
+#b = int(input())
+#count = 0
+#for i in range(a, b + 1):
+#  print(i)
+#  count += 1
+#  print("soni =" , count)
+ 
